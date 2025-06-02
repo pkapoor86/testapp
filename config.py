2f5308373e6c750f3a692adb15f781aa7dfb3952
root <root@localhost.localdomain>
2025-06-02 08:32:24 -0400

    initial Commit 4

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev-key'
-    SQLALCHEMY_DATABASE_URI = "mysql://inventorydb:Inv3ntory#DB!2025@localhost/inventorydb"
+    SQLALCHEMY_DATABASE_URI = 'mysql://inventorydb:Inv3ntory#DB!2025@localhost/inventorydb'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
